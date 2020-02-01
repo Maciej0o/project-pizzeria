3aec6eb05f959d848cbcc3685891430f1acfb25a
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-02-01 11:51:57 +0100

    Add page booking

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,65 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(idFromHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activatepage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      })
+    }
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /*add class "active" to matching pages, rwmove from non-matching */
+    for(let page of thisApp.pages){
+      //if(page.id == pageId){
+      //  page.classList.add(classNames.pages.active);
+      //}else{
+      //page.classList.remove(classNames.pages.active);
+      //}
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+      /* add class "active" to matching links, remove from non-matching */
+      for(let link of thisApp.navLinks){
+          link.classList.toggle(
+            classNames.nav.active,
+            link.getAttribute('href') == '#' + pageId
+          );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     //console.log('thisApp.data:', thisApp.data);
@@ -55,6 +114,8 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    thisApp.initPages();
 
     thisApp.initData();
     thisApp.initCart();
